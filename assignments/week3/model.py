d210dbea7a9a2e997cb52788f1429adea3841f7a
Chakradhar Paladi <90115550+Spybuoy@users.noreply.github.com>
2023-02-08 03:50:57 -0500

    Update model.py

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,15 +1,21 @@
+import torch
 from typing import Callable
+import torch.nn as nn
 
 
-class MLP:
+class MLP(nn.Module):
+    """
+    This is the MLP module
+    """
+
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
         num_classes: int,
         hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
-        initializer: Callable = torch.nn.init.ones_,
+        activation: Callable = nn.ReLU,
+        initializer: Callable = nn.init.ones_,
     ) -> None:
         """
         Initialize the MLP.
@@ -21,9 +27,14 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        """
+        this is the __init__
+        """
+        super(MLP, self).__init__()
+        self.fc1 = torch.nn.Linear(input_size, hidden_size)
+        self.fc2 = torch.nn.Linear(hidden_size, num_classes)
 
-    def forward(self, x):
+    def forward(self, x: torch.tensor) -> torch.tensor:
         """
         Forward pass of the network.
 
@@ -33,4 +44,9 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        """
+        foward pass
+        """
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
