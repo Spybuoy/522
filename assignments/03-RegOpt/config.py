b93ee534d0f475edb47178271294da747e7230bb
Spybuoy <paladi.chakradhar01@gmail.com>
2023-02-19 21:39:37 -0500

    feb 19 21:39

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,29 +2,41 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision.transforms import (
+    Compose,
+    Normalize,
+    ToTensor,
+)
 
 
 class CONFIG:
-
-    batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
-    initial_weight_decay = 0
+    batch_size = 256
+    num_epochs = 7
+    initial_learning_rate = 0.006
+    initial_weight_decay = 0.0001
+    lr_factor = 0.001
+    lr_patience = 10
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "lr_factor": lr_factor,
+        "lr_patience": lr_patience,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
         [
             ToTensor(),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            # RandomHorizontalFlip(),
+            # RandomCrop(32, padding=4),
         ]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,21 +4,66 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    This is the CustomLRScheduler class
+    """
+
+    def __init__(self, optimizer, last_epoch=-1, lr_factor=0.1, lr_patience=10):
         """
         Create a new scheduler.
 
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
-
+        Args:
+            optimizer (torch.optim.Optimizer): Wrapped optimizer.
+            last_epoch (int): The index of last epoch.
+            lr_factor (float): The factor by which the learning rate will be reduced.
+            lr_patience (int): The number of epochs with no improvement after which learning rate will be reduced.
         """
-        # ... Your Code Here ...
+        self.lr_factor = lr_factor
+        self.lr_patience = lr_patience
+        self.counter = 0
+        self.best_loss = float("inf")
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
+        """
+        This is the get_lr method
+        """
+        """
+        Compute the learning rate for each parameter group.
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        Returns:
+            List[float]: The computed learning rates.
+
+        """
+        if self.last_epoch == 0:
+            return self.base_lrs
+
+        if self.counter >= self.lr_patience:
+            for param_group in self.optimizer.param_groups:
+                param_group["lr"] *= self.lr_factor
+            self.counter = 0
+        print([group["lr"] for group in self.optimizer.param_groups])
+        return [group["lr"] for group in self.optimizer.param_groups]
+
+    def step(self, epoch=None, metrics=None):
+        """
+        Update the learning rate for the next epoch.
+
+        Args:
+            epoch (int): The index of the current epoch. If None, uses self.last_epoch + 1. Default: None.
+            metrics (dict): A dictionary of metrics for the current epoch. Default: None.
+
+        """
+        if metrics is None:
+            self.last_epoch += 1
+        else:
+            loss = metrics.get("loss")
+            if loss < self.best_loss:
+                self.best_loss = loss
+                self.counter = 0
+            else:
+                self.counter += 1
+
+            self.last_epoch = epoch
+
+        self._last_lr = [group["lr"] for group in self.optimizer.param_groups]
